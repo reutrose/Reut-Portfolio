0573ffcdb56a7ee5e4f26dd8f62b7d9396b63ce8
reutrose <reutroz@gmail.com>
2025-02-14 10:08:28 +0200

    navbar settings

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -33,8 +33,6 @@ function Navbar() {
 						<span className="navbar-toggler-icon"></span>
 					</button>
 					<div className="collapse navbar-collapse" id="navbarSupportedContent">
-						<ul className="navbar-nav me-auto mb-2 mb-lg-0 d-flex w-100"></ul>
-						<ul className="navbar-nav me-auto mb-2 mb-lg-0 d-flex w-100"></ul>
 						<ul className="navbar-nav me-auto mb-2 mb-lg-0 d-flex justify-content-end w-100 uppercase align-items-center">
 							<li className="nav-item me-4 text-center">
 								<a
